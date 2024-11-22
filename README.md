c3a9090911cf99d75563b63faf5aa7e1efdde1b9
jeevan-cmd836 <jeevanrabadiya75@gmail.com>
2024-11-22 12:43:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Motivallary.com
+# Untitled
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Jeevan-Rabadiya/pen/LYwwNwG](https://codepen.io/Jeevan-Rabadiya/pen/LYwwNwG).
+
---
+++ script.js
@@ -0,0 +1,11 @@
+// Placeholder for potential JavaScript functionality
+
+console.log("Welcome to Motivallari Pearl Vines!");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Example: alert when page loads
+
+    alert("Welcome to Motivallari Pearl Vines!");
+
+});
